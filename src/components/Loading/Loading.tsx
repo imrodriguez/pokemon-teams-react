55c7e95e8cbcc9c568835c13c6b3cdd6a6e5a93a
imanol Rodríguez de maya <imanolrodriguezdemaya@imanols-MacBook-Pro.local>
2023-02-10 10:37:25 +0100

    change basePath

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import styles from "./Loading.module.css"
 
 export const Loading = () => (
     <div className={styles.Loading}>
-        <img src="/pokeball.png" className="pokeball" alt="pokeball" />
+        <img src="/pokemon-teams-react/pokeball.png" className="pokeball" alt="pokeball" />
         <h1>Loading...</h1>
     </div>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pokemon-teams-react/",
   plugins: [react()],
   server: {
     port: 3000,
